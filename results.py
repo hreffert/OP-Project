1fd842dd3709fb3a693e0b0c8f2c4455528c4fd1
Akhil <akhiltulip@gmail.com>
2023-06-09 16:54:27 +0530

    s

--- results.py
+++ results.py
@@ -4,12 +4,28 @@ import common
 proxyObj = common.ProxyServer()
 
 
+def eachYear(url):
+  print(url)
+
 def eachResults(url):
-  exit()
+  league = proxyObj.get_request(url)
+  yearsSelect = common.getValue('<select class="int-select(.*?)</select', league.text)
+  if yearsSelect is not None:
+    years =   common.getValue('value="(.*?)"', league.text, "yes")
+    if years is not None:
+      for year in years:
+        if url != year:
+          eachYear(year, league.text)
+        else:
+          eachYear(year)
+    else:
+      print("Years not found")      
+  else:
+    print("Years Select not found")
 
-
-eachResults("https://www.oddsportal.com/football/africa/africa-cup-of-nations/results/")
-exit()
+#eachResults("https://www.oddsportal.com/football/albania/albanian-cup/results/")
+#eachYear("https://www.oddsportal.com/football/albania/albanian-cup-2021-2022/results/")
+#exit()
 
 results = proxyObj.get_request("https://www.oddsportal.com/football/results/")
 links = common.getValue('<li class="flex items-center.*?href="(.*?)"', results.text, "yes")
