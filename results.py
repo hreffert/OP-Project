a56f8585a2ccf68387db72c8dec7908664c40ef8
jeweljinan <jeweljinan@gmail.com>
2023-06-12 19:50:06 +0530

    new

--- results.py
+++ results.py
@@ -1,9 +1,10 @@
 import common 
 from datetime import datetime
 import json
+import html
 
 proxyObj = common.ProxyServer()
-
+site = 'https://www.oddsportal.com'
 
 def eachYear(url, out=None):
   if out is None:
@@ -11,6 +12,7 @@ def eachYear(url, out=None):
     out = yearD.text
   
   idV = common.getValue('"id":"(.*?)"', out)
+  bets(idV)
   if idV is not None:
     print("Id is: ", idV)
     jsonUrl = "https://www.oddsportal.com/ajax-sport-country-tournament-archive_/1/"+str(idV)+"/X0/1/0/?_="+str(round(datetime.now().timestamp()))
@@ -36,7 +38,7 @@ def eachResults(url):
   if yearsSelect is not None:
     years =   common.getValue('value="(.*?)"', league.text, "yes")
     if years is not None:
-      for year in years:
+      for year in years:  
         if url == year:
           eachYear(year, league.text)
         else:
@@ -45,10 +47,42 @@ def eachResults(url):
       print("Years not found")      
   else:
     print("Years Select not found")
-
+    
+def eachRaces(id):
+  jsonUrl = f'https://www.oddsportal.com/ajax-sport-country-tournament-archive_/1/{id}/X0/1/0/?_={str(round(datetime.now().timestamp()))}'
+  jsonVal = proxyObj.get_request(jsonUrl)
+  try:
+    jsonData = jsonVal.json()
+    jsonVal = str(jsonData)
+  except Exception as ex:
+    print('Forbidden')
+    exit()
+  #'''
+  if jsonVal != "Forbidden":
+    for x in jsonData['d']['rows']:
+      link = site + x['url']
+      print(link)
+    '''
+    urls = common.getValue("({'id'.*?1-1-0-0'})", jsonVal , "yes")
+    #print(urls)
+    #exit()
+    for x in urls:
+      print(x)
+      exit()
+    '''
+  #'''
+  
+def bets(url):
+  r = rq.req(url)
+  print(r)
+  
 #eachResults("https://www.oddsportal.com/football/albania/albanian-cup/results/")
 #eachYear("https://www.oddsportal.com/football/africa/africa-cup-of-nations/results/")
-#exit()
+#eachRaces('6Jzuq5IH')
+rq = common.request(1,1)
+rq.startDrivers()
+bets('https://www.oddsportal.com/football/africa/africa-cup-of-nations/rwanda-benin-AXWn6ma0/')
+exit()
 
 results = proxyObj.get_request("https://www.oddsportal.com/football/results/")
 links = common.getValue('<li class="flex items-center.*?href="(.*?)"', results.text, "yes")
