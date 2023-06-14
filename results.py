edae8d6349b94426220d560304c736dc8d33bb32
homie <jeweljinan>
2023-06-14 19:16:26 +0530

    change

--- results.py
+++ results.py
@@ -3,168 +3,241 @@ from datetime import datetime
 import json
 import html
 import time
+import concurrent.futures
+from configparser import ConfigParser
+
+
+config_object = ConfigParser()
+config_object.read("config.ini")
+
+#threadcounts
+threadCounts = config_object['ThreadCounts']
+eachResultsT_count = int(threadCounts['eachResult'])
+eachYearT_count = int(threadCounts['eachYear'])
+
+#Credentials
+credentails = config_object['Credentials']
+mongoUri = credentails['mongo']
 
 proxyObj = common.ProxyServer()
+
+headers = {
+  'accept': 'application/json, text/plain, */*',
+  'authority': 'www.oddsportal.com',
+  'Accept-Language': 'en-US,en;q=0.5',
+  'X-Requested-With': 'XMLHttpRequest',
+  'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+}
+
+
 site = 'https://www.oddsportal.com'
 
-def eachYear(url, out=None):
-  if out is None:
-    yearD = proxyObj.get_request(url)
-    out = yearD.text
+def makeRequest(url, referer=False):
+  nHeders = headers.copy()
+  if referer:
+    nHeders['Referer'] = referer
   
-  idV = common.getValue('"id":"(.*?)"', out)
-  if idV is not None:
-    print("Id is: ", idV)
-    jsonUrl = "https://www.oddsportal.com/ajax-sport-country-tournament-archive_/1/"+str(idV)+"/X0/1/0/?_="+str(round(datetime.now().timestamp()))
-    jsonVal = proxyObj.get_request(jsonUrl)
-    jsonDec = jsonVal.text
-    if jsonDec != "Forbidden.":
-      jsonDec = json.loads(jsonDec)
-      for one in jsonDec["d"]["rows"]:
-        print("Time:", one['date-start-base'])
-        print("Home:", one['home-name'])
-        print("Away:", one['away-name'])
-        print("H Score:", one['homeResult'])
-        print("A Score:", one['awayResult'])
-        print("\n-------------------------------------------------------------------\n")
+  for i in range(5):
+    try:
+      print(f"Try No: {i+1} on url: {url}")
+      r = proxyObj.get_request(url, nHeders)
+      print(r)
+      if r.text == "Forbidden.":
+        time.sleep(3)
+        continue
+      return r
+    except Exception as ex:
+      print(f"EX in request: {ex}, datetime: {datetime.now()}")
+  exit("NO DATA")
+  
+def eachYear(url, out=None):
+  data = {"yearURL":url, 'races':[]}
+  try:
+    if out is None:
+      yearD = makeRequest(url)
+      out = yearD.text
+    
+    idV = common.getValue('"id":"(.*?)"', out)
+    if idV is not None:
+      try:
+        print("Id is: ", idV)
+        jsonUrl = "https://www.oddsportal.com/ajax-sport-country-tournament-archive_/1/"+str(idV)+"/X0/1/0/?_="+str(round(datetime.now().timestamp()))
+        jsonVal = makeRequest(jsonUrl, url)
+        jsonDec = jsonVal.text
+        if jsonDec != "Forbidden.":
+          jsonDec = json.loads(jsonDec)
+          for one in jsonDec["d"]["rows"]:
+            url = site + one['url']
+            '''
+            print("Url:", url)
+            print("Odds:", bets(url))
+            print("Time:", one['date-start-base'])
+            print("Home:", one['home-name'])
+            print("Away:", one['away-name'])
+            print("H Score:", one['homeResult'])
+            print("A Score:", one['awayResult'])
+            '''
+            print("\n-------------------------------------------------------------------\n")
+            data['races'].append({'raceURL':url, 'Odds':bets(url), 'Time':one['date-start-base'], 'Home':one['home-name'], 'Away': one['away-name'], 'H Score':one['homeResult'], 'A Score':one['awayResult']})
+            #break #testing
+        else:
+          print("Error: ", jsonDec)
+      except Exception as ex:
+        print(f"EX: {ex}, eachYear inside tryExcept")
+      collection.insert_one(data)
+      print("UPDATED INTO DB")
     else:
-      print("Error: ", jsonDec)
-  else:
-    print("Id not found")  
+      print("Id not found")  
+  except Exception as ex:
+    print(f"EX: {ex} in eachYear, url: {url}, datetime: {datetime.now()}")
+  
 
 def eachResults(url):
-  league = proxyObj.get_request(url)
-  yearsSelect = common.getValue('<select class="int-select(.*?)</select', league.text)
-  if yearsSelect is not None:
-    years =   common.getValue('value="(.*?)"', league.text, "yes")
-    if years is not None:
-      for year in years:  
-        if url == year:
-          eachYear(year, league.text)
-        else:
-          eachYear(year)
+  try:
+    league = makeRequest(url)
+    yearsSelect = common.getValue('<select class="int-select(.*?)</select', league.text)
+    if yearsSelect is not None:
+      years =   common.getValue('value="(.*?)"', league.text, "yes")
+      if years is not None:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=eachYearT_count) as executor:
+          for year in years:  
+            if url == year:
+              executor.submit(eachYear, year, league.text)
+            else:
+              executor.submit(year)
+            #break #testing
+      else:
+        print("Years not found")      
     else:
-      print("Years not found")      
-  else:
-    print("Years Select not found")
+      print("Years Select not found")
+  except Exception as ex:
+    print(f"EX: {ex} in eachResult, url: {url}, datetime: {datetime.now()}")
   
 def bets(url):
-  slug = [x for x in url.split('-') if x != ''][-1]
-  slug = slug.replace('/', '')
-  
-  proxyObj.user_agent['Referer'] = url
-  proxyObj.user_agent['accept'] = 'application/json, text/plain, */*'
-  proxyObj.user_agent['authority'] = 'www.oddsportal.com'
-  proxyObj.user_agent['Accept-Language'] = 'en-US,en;q=0.5'
-  proxyObj.user_agent['X-Requested-With'] = 'XMLHttpRequest'
-  #proxyObj.user_agent['authority'] = 'www.oddsportal.com'
-  siteHtml = proxyObj.get_request(url)
-  oddsId = html.unescape(common.getValue('<Event :data="(.*?)"', siteHtml.text)).replace('&nbsp;', ' ')
-  oddsId = json.loads(oddsId)['eventBody']['providersNames']
-  
   newobj = {'1X2':{}, 'Over/Under':{}, 'Asian Handicap':{}, 'BTTS':{}}
+  try:
+    slug = [x for x in url.split('-') if x != ''][-1]
+    slug = slug.replace('/', '')
+    
+    siteHtml = makeRequest(url)
+    oddsId = html.unescape(common.getValue('<Event :data="(.*?)"', siteHtml.text)).replace('&nbsp;', ' ')
+    oddsId = json.loads(oddsId)['eventBody']['providersNames']
+    
+    try:
+      #1X2
+      fulltime = getOdds(slug, oddsId, 2, 1, '1X2', url) 
+      fHalf = getOdds(slug, oddsId, 3, 1, '1X2', url)
+      sHalf = getOdds(slug, oddsId, 4, 1, '1X2', url)
+      newobj['1X2'] = setData(oddsId, fulltime, fHalf, sHalf)
+    except Exception as ex:
+      print(f"EX: {ex} in 1X2, url: {url}, datetime: {datetime.now()}")
+
+    try:
+      #over/under
+      fulltime = getOdds(slug, oddsId, 2, 2, 'over/under', url)
+      fHalf = getOdds(slug, oddsId, 3, 2, 'over/under', url)
+      sHalf = getOdds(slug, oddsId, 4, 2, 'over/under', url)
+      newobj['Over/Under'] = setData(oddsId, fulltime, fHalf, sHalf, "yes")
+    except Exception as ex:
+      print(f"EX: {ex} in over/under, url: {url}, datetime: {datetime.now()}")
+
+    try:
+      #asianHandicap
+      fulltime = getOdds(slug, oddsId, 2, 5, 'Asian Handicap', url)
+      fHalf = getOdds(slug, oddsId, 3, 5, 'Asian Handicap', url)
+      sHalf = getOdds(slug, oddsId, 4, 5, 'Asian Handicap', url)
+      newobj['Asian Handicap'] = setData(oddsId, fulltime, fHalf, sHalf, "yes")
+    except Exception as ex:
+      print(f"EX: {ex} in asianHandicap, url: {url}, datetime: {datetime.now()}")
+    
+    try:
+      #bothteamscore
+      fulltime = getOdds(slug, oddsId, 2, 13, 'BTTS', url)
+      fHalf = getOdds(slug, oddsId, 3, 13, 'BTTS', url)
+      sHalf = getOdds(slug, oddsId, 4, 13, 'BTTS', url)
+      newobj['BTTS'] = setData(oddsId, fulltime, fHalf, sHalf)
+    except Exception as ex:
+      print(f"EX: {ex} in bothteamscore, url: {url}, datetime: {datetime.now()}")
+      
+  except Exception as ex:
+    print(f"EX: {ex}, FUNCTION: bets, URL: {url}, datetime: {datetime.now()}")
   
-  #1X2
-  fulltime = getOdds(slug, oddsId, 2, 1, '1X2') 
-  fHalf = getOdds(slug, oddsId, 3, 1, '1X2')
-  sHalf = getOdds(slug, oddsId, 4, 1, '1X2')
-  newobj['1X2'] = setData(oddsId, fulltime, fHalf, sHalf)
-
-  #over/under
-  fulltime = getOdds(slug, oddsId, 2, 2, 'over/under')
-  fHalf = getOdds(slug, oddsId, 3, 2, 'over/under')
-  sHalf = getOdds(slug, oddsId, 4, 2, 'over/under')
-  newobj['Over/Under'] = setData(oddsId, fulltime, fHalf, sHalf, "yes")
-
-  #asianHandicap
-  fulltime = getOdds(slug, oddsId, 2, 5, 'Asian Handicap')
-  fHalf = getOdds(slug, oddsId, 3, 5, 'Asian Handicap')
-  sHalf = getOdds(slug, oddsId, 4, 5, 'Asian Handicap')
-  newobj['Asian Handicap'] = setData(oddsId, fulltime, fHalf, sHalf, "yes")
-  
-  #bothteamscore
-  fulltime = getOdds(slug, oddsId, 2, 13, 'BTTS')
-  fHalf = getOdds(slug, oddsId, 3, 13, 'BTTS')
-  sHalf = getOdds(slug, oddsId, 4, 13, 'BTTS')
-  newobj['BTTS'] = setData(oddsId, fulltime, fHalf, sHalf)
-
   return newobj
       
-def getOdds(slug, oddsId, gTime, gType, dType):
-  #print("KAKAKAK", slug, oddsId, gTime, gType)
-  link = f'https://www.oddsportal.com/feed/match-event/1-1-{slug}-{gType}-{gTime}-yja83.dat'
-  for _ in range(5):
-    try:
-      oddsJson = proxyObj.get_request(link)
-      if 'Forbidden.' in oddsJson.text:
-        exit('Forbidden.')
-      oddsJson = oddsJson.json()
-      break
-    except Exception as ex:
-      print(ex)
-      time.sleep(5)
-      pass
-  
-  
-  
-  if dType in ['1X2', 'BTTS']:
-    obj = {}
-    oddsData = oddsJson['d']['oddsdata']['back'][f'E-{gType}-{gTime}-0-0-0']['odds']
-    for key, value in oddsId.items():
-      if key in oddsData:
-        obj[value] = oddsData[key]
-    return obj
-    
-  if dType in ['Asian Handicap', 'over/under']:
-    obj = {}
-    for x in ['0.5', 1.5, 2.5, 3.5]:
-      obj[x] = {}
-      if f'E-{gType}-{gTime}-0-{x}-0' in oddsJson['d']['oddsdata']['back']:
-        oddsData = oddsJson['d']['oddsdata']['back'][f'E-{gType}-{gTime}-0-{x}-0']['odds']
-        for key, value in oddsId.items():
-          if key in oddsData:
-            obj[x][value] = oddsData[key]
-    return obj
-  
+def getOdds(slug, oddsId, gTime, gType, dType, referer):
+  try:
+    link = f'https://www.oddsportal.com/feed/match-event/1-1-{slug}-{gType}-{gTime}-yja83.dat'
+    oddsJson = makeRequest(link, referer)
+    oddsJson = oddsJson.json()
+      
+    if dType in ['1X2', 'BTTS']:
+      obj = {}
+      oddsData = oddsJson['d']['oddsdata']['back'][f'E-{gType}-{gTime}-0-0-0']['odds']
+      for key, value in oddsId.items():
+        if key in oddsData:
+          obj[value] = oddsData[key]
+      return obj
+      
+    if dType in ['Asian Handicap', 'over/under']:
+      obj = {}
+      for x in ['0.5', '1.5', '2.5', '3.5']:
+        obj[x] = {}
+        if f'E-{gType}-{gTime}-0-{x}-0' in oddsJson['d']['oddsdata']['back']:
+          oddsData = oddsJson['d']['oddsdata']['back'][f'E-{gType}-{gTime}-0-{x}-0']['odds']
+          for key, value in oddsId.items():
+            if key in oddsData:
+              obj[x][value] = oddsData[key]
+      return obj
+  except Exception as ex:
+    print(f"EX: {ex}, FUNCTION: getOdds, datetime: {datetime.now()}")
+  return {}
   
 def setData(oddsId, fulltime, fHalf, sHalf, objC='No'):
   obj = {}
-  for key in list(oddsId.values()):
-    obj[key] = {'fulltime': {}, 'firstHalf': {}, 'secondHalf': {}}
-    if objC == "yes":
-      for x in ['0.5', 1.5, 2.5, 3.5]:
-        if key in fulltime[x]:
-          obj[key]['fulltime'][x] = fulltime[x][key]
-        if key in fHalf[x]:
-          obj[key]['firstHalf'][x] = fHalf[x][key]
-        if key in sHalf[x]:
-          obj[key]['secondHalf'][x] = sHalf[x][key]
-    else:
-      if key in fulltime:
-        obj[key]['fulltime'] = fulltime[key]
-      if key in fHalf:
-        obj[key]['firstHalf'] = fHalf[key]
-      if key in sHalf:
-        obj[key]['secondHalf'] = sHalf[key]
+  try:
+    for key in list(oddsId.values()):
+      obj[key] = {'fulltime': {}, 'firstHalf': {}, 'secondHalf': {}}
+      if objC == "yes":
+        for x in ['0.5', '1.5', '2.5', '3.5']:
+          if key in fulltime[x]:
+            obj[key]['fulltime'][x] = fulltime[x][key]
+          if key in fHalf[x]:
+            obj[key]['firstHalf'][x] = fHalf[x][key]
+          if key in sHalf[x]:
+            obj[key]['secondHalf'][x] = sHalf[x][key]
+      else:
+        if key in fulltime:
+          obj[key]['fulltime'] = fulltime[key]
+        if key in fHalf:
+          obj[key]['firstHalf'] = fHalf[key]
+        if key in sHalf:
+          obj[key]['secondHalf'] = sHalf[key]
+  except Exception as ex:
+    print(f"EX: {ex}, FUNCTION: setData, datetime: {datetime.now()}")
   return obj
       
-  
+
+db = common.get_mongoDb(mongoUri)
+collection = db.TEST 
 #eachResults("https://www.oddsportal.com/football/albania/albanian-cup/results/")
-#eachYear("https://www.oddsportal.com/football/africa/africa-cup-of-nations/results/")
-a = bets('https://www.oddsportal.com/football/africa/africa-cup-of-nations/rwanda-benin-AXWn6ma0/')
-print(a)
+eachYear("https://www.oddsportal.com/football/africa/africa-cup-of-nations/results/")
+#a = bets('https://www.oddsportal.com/football/africa/africa-cup-of-nations/rwanda-benin-AXWn6ma0/')
+#print(a)
 exit()
 
-results = proxyObj.get_request("https://www.oddsportal.com/football/results/")
+db = common.get_mongoDb(mongoUri)
+collection = db.TEST
+results = makeRequest("https://www.oddsportal.com/football/results/")
 links = common.getValue('<li class="flex items-center.*?href="(.*?)"', results.text, "yes")
 print("Total links found: ", len(links))
 
-
-for one in links:
-  print("Scrap match: ", one)
-  validate = common.getValue('^/football/(\w+.*?)$', one)
-  if validate is not None:
-    eachResults("https://www.oddsportal.com"+str(one))
-  else:
-    print("Invalid match link", one)
+with concurrent.futures.ThreadPoolExecutor(max_workers=eachResultsT_count) as executor:
+  for one in links:
+    print("Scrap match: ", one)
+    validate = common.getValue('^/football/(\w+.*?)$', one)
+    if validate is not None:
+      executor.submit(eachResults, site + str(one))
+    else:
+      print("Invalid match link", one)
+    #break #testing
     
