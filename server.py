25770a90acf0746011ead3efe8cd2d3056a403fc
homie <jeweljinan>
2023-06-24 18:11:50 +0530

    fix

--- server.py
+++ server.py
@@ -32,17 +32,25 @@ def index():
                                     }
                                   }
                                 },
-                                'seasons':{'$reduce':{'input':'$races', 'initialValue':[],'in':{'$concatArrays':["$$value",["$$this.season"]]}}},
-                                '_id': 0
+                                'league':{'$reduce':{'input':'$races', 'initialValue':[],'in':{'$concatArrays':["$$value",["$$this.league"]]}}},
+                                '_id': 0,
+                                'year':1,
                               }
                             }
                           ])
   countryDD = []
-  seasonsDD = []
+  leagueDD = []
+  yearsDD = []
   for item in cur:
-    [countryDD.append(x) for x in item['races'] if x not in countryDD]
-    [seasonsDD.append(x) for x in item['seasons'] if x not in seasonsDD]
-  return render_template('index.html', data={"country":countryDD, "seasons":seasonsDD})
+    if type(item.get('races')) == list:
+      [countryDD.append(x) for x in item['races'] if x not in countryDD and x not in ['', ' ', None]]
+    if type(item.get('league')) == list:
+      [leagueDD.append(x) for x in item['league'] if x not in leagueDD and x not in ['', ' ', None]]
+    y = item.get('year')
+    if y not in [None, '', ' '] and y not in yearsDD:
+      yearsDD.append(y)
+
+  return render_template('index.html', data={"country":countryDD, "league":leagueDD, "seasons":sorted(yearsDD)})
   
 @app.route('/filter', methods = ['GET'])
 def filter():
@@ -50,6 +58,11 @@ def filter():
   league = request.args.get('league').split(',')
   dataFrame = request.args.get('df').split(',')
   seasons = request.args.get('season').split(',')
+  oddsCompare = request.args.get('oddsCompare')
+  oddsTime = request.args.get('oddsTime')
+  minOdds = request.args.get('minOdds')
+  maxOdds = request.args.get('maxOdds')
+  
   
   country = [re.compile(e.strip(), re.I) for e in country if e not in ['', ' ']]
   league = [x.strip() for x in league if x not in ['', ' ']]
@@ -70,9 +83,9 @@ def filter():
     if country != []:
       q[0]['$match']['races']['$elemMatch']['$and'].append({'country':{'$in':country}})
     if league != []:
-      q[0]['$match']['races']['$elemMatch']['$and'].append({'game':{'$in':league}})
+      q[0]['$match']['races']['$elemMatch']['$and'].append({'league':{'$in':league}})
     if seasons != []:
-      q[0]['$match']['races']['$elemMatch']['$and'].append({'season':{'$in':seasons}})
+      q[0]['$match']['year'] = {'$in':seasons}
       
     if dataFrame != []:
       obj = {}
@@ -102,10 +115,13 @@ def filter():
   q[2]['$match'] = matchAnd
 
   if len(q[0]['$match']['races']['$elemMatch']['$and']) == 0:
-    q = [{'$project':{'_id':0}}]
+    if 'year' not in q[0]['$match']:
+      q = [{'$project':{'_id':0}}]
+    else:
+      q[0]['$match'].pop('races')
     
   print("QUERY:", q)
-  a = db.TEST.aggregate(q)
+  a = db.ODDS.aggregate(q)
   data = [x for x in a]
   return {"status":200, "data":data}
   
@@ -114,11 +130,11 @@ def startScrap():
   script = request.args.get('script')
   EXECUTOR = concurrent.futures.ThreadPoolExecutor()
   if 'tResults' == script:
-    EXECUTOR.submit(todaysMatchs.start)
+    EXECUTOR.submit(todaysMatchs.start, db)
   elif 'results' == script:
-    EXECUTOR.submit(results.start)
+    EXECUTOR.submit(results.start, db)
   return {"status":200, "data":"SCRAPING HAS STARTED"}
   
 if __name__ == '__main__':
-  app.run(app, debug=True)
+  app.run(host='0.0.0.0',port='8888', debug=True)
 
