805e3f5e06a9d2e166f8b9e33f6156cd6e6a115c
homie <jeweljinan>
2023-06-28 20:07:46 +0530

    fix

--- server.py
+++ server.py
@@ -136,5 +136,5 @@ def startScrap():
   return {"status":200, "data":"SCRAPING HAS STARTED"}
   
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',port='8888', debug=True)
+  app.run(host='0.0.0.0',port='80', debug=True)
 
